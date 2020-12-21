9a36c2bc8266ba4a19509497c66f60b7b3653447
Pranjal <pranjaljsl2910@gmail.com>
2020-12-22 03:31:54 +0530

    home controller async added

--- controllers/home_controller_action.js
+++ controllers/home_controller_action.js
@@ -3,36 +3,60 @@ const user = require('../models/user');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (req, res) {
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
 
-    // populate the user of each post ie fill all data of user not only ObjectID
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        }).exec(function (err, posts) {
-            User.find({},function(err,userss){
-                return res.render('home', {
-                    title: "MernSocial | Home",
-                    posts: posts,
-                    allUser:userss
-                });
-            })
-           
-        })
-
+module.exports.home = async function (req, res) {
+    try {
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            });
+        let userss = await User.find({});
+        return res.render('home', {
+            title: "MernSocial | Home",
+            posts: posts,
+            allUser: userss
+        });
+    } catch (error) {
+        console.log('error', error);
+        return;
+    }
 }
+
+// module.exports.home = function (req, res) {
+//     // console.log(req.cookies);
+//     // res.cookie('user_id', 25);
+
+//     // Post.find({}, function(err, posts){
+//     //     return res.render('home', {
+//     //         title: "Codeial | Home",
+//     //         posts:  posts
+//     //     });
+//     // });
+
+//     // populate the user of each post ie fill all data of user not only ObjectID
+//     Post.find({})
+//         .populate('user')
+//         .populate({
+//             path: 'comments',
+//             populate: {
+//                 path: 'user'
+//             }
+//         }).exec(function (err, posts) {
+//             User.find({}, function (err, userss) {
+//                 return res.render('home', {
+//                     title: "MernSocial | Home",
+//                     posts: posts,
+//                     allUser: userss
+//                 });
+//             })
+
+//         })
+
+// }
 
 // module.exports.actionName = function(req, res){}
